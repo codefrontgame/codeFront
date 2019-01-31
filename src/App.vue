22bc2b3ecec96115420a17d3cc7a35f5e66e65ef
Karl 'NaN' Wikström <karlwik@student.chalmers.se>
2019-01-31 11:57:15 +0100

    Remove unwanted template code and add test button

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,26 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <input type="text" v-model="userCode">
+    <input type="button" @click="run" value="knapptext">
   </div>
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld.vue";
-
 export default {
   name: "app",
-  components: {
-    HelloWorld
+  data() {
+    return {
+      userCode: ""
+    }
+  },
+  methods: {
+    run() {
+      console.log(this.userCode)
+    }
   }
 };
 </script>
 
 <style lang="scss">
-#app {
-  font-family: "Avenir", Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
 </style>
