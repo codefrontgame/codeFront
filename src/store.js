b60a6c89ad46219c07d8ee16af9d0b3bce5e4a72
Gurgy <gurgy@emycia.com>
2019-02-25 21:59:23 +0100

    Add structure skiss in store

--- src/store.js
+++ src/store.js
@@ -4,7 +4,47 @@ import Vuex from 'vuex'
 Vue.use(Vuex)
 
 export default new Vuex.Store({
-  state: {},
+  state: {
+    characters: {
+      zombie: {
+        userFunctions: {
+          move: {
+            cn: 'Move',
+            userCode: '',
+          },
+        },
+        graphics: { // built from directory structure?
+          north: {
+            running: [
+              // running north frames (preloaded)
+            ],
+            still: [
+              // standing still north frames
+            ],
+          }, // ...
+          mugshot: '', // File
+        },
+      },
+    },
+    entities: {
+      character: 'zombie', // pointer?
+      x: 0,
+      y: 0,
+      health: 100,
+      update ({ userFunctions, board, entities, ticks }) {
+        //
+      },
+      draw ({ board, sketch }) {
+
+      },
+    },
+    board: {
+      // ... properties of the board
+    },
+  },
+  getters: {},
   mutations: {},
   actions: {},
 })
+
+// helper function to call user code
