187b6c24f4b0b95f94aa7170d366e9469fbfa1b7
Gurgy <gurgy@emycia.com>
2019-02-27 16:19:54 +0100

    Move zombie acording to user code

--- src/components/CharacterFunctions.vue
+++ src/components/CharacterFunctions.vue
@@ -20,7 +20,7 @@ export default {
   data () {
     return {
       expanded: [],
-      defaultExpansion: false,
+      defaultExpansion: true,
     }
   },
   props: {
--- src/components/GameBoard.vue
+++ src/components/GameBoard.vue
@@ -17,51 +17,16 @@ export default {
     return {
       assets: [],
       fr: 36,
-      entities: [
-        {
-          // x, y is middle of foot base
-          x: 0, // 0 - xTiles
-          y: 0, // 0 - yTiles
-          size: 80,
-          dx: 0.2,
-          draw ({ sketch, assets, self}) {
-            let img = assets['zombie']
-            let xTiles = 10
-            let yTiles = 15
-            let closeCover = 0.8
-            let farCover = 0.4
-            let start = 0
-            let end = 0.9
-
-            let realY = sketch.height - (this.y * (end - start) * (sketch.height / yTiles))
-            let realX = ((this.x - (xTiles / 2)) * (closeCover + (((farCover - closeCover) / yTiles) * this.y)) + ((xTiles / 2))) * (sketch.width / xTiles)
-            let perspectiveSize = this.size * (closeCover + (((farCover - closeCover) / yTiles) * this.y))
-            let changeFactor = perspectiveSize / img.width
-
-            sketch.image(img, realX - (this.size / 2), realY - (img.height * changeFactor), img.width * changeFactor, img.height * changeFactor)
-          },
-          update ({ ticks, self }) {
-            // make the entity follow the border of the playing field
-            let change = 1 * ticks
-            if (this.x <= 0 && this.y >= 15) {
-              this.x += change
-            } else if (this.x <= 0) {
-              this.y += change
-            } else if (this.y >= 15 && this.x >= 10) {
-              this.y -= change
-            } else if (this.y >= 15) {
-              this.x += change
-            } else if (this.y <= 0 && this.x >= 10) {
-              this.x -= change
-            } else if (this.x >= 10) {
-              this.y -= change
-            } else if (this.y <= 0) {
-              this.x -= change
-            }
-          },
-        },
-      ],
+      board: {
+        xTiles: 10,
+        yTiles: 15,
+      },
     }
+  },
+  computed: {
+    entities () {
+      return this.$store.getters['getEntities']
+    },
   },
   components: {
     VueP5,
@@ -86,12 +51,15 @@ export default {
         this.entities[i].update({
           sketch: sketch,
           ticks: 1 / fr,
+          board: this.board,
         })
       }
       for (let i = 0; i < this.entities.length; i++) {
         this.entities[i].draw({
           sketch: sketch,
           assets: this.assets,
+          ticks: 1 / fr,
+          board: this.board,
         })
       }
     },
