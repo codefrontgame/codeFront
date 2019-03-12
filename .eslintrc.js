8f8ec822ef17e02819adf6e0e85c269dae210902
Gurgy <gurgy@emycia.com>
2019-03-12 14:27:52 +0100

    Solve build error

--- .eslintrc.js
+++ .eslintrc.js
@@ -6,7 +6,7 @@ module.exports = {
   extends: ['plugin:vue/essential', 'standard'],
   rules: {
     'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
-    'no-debugger': process.env.NODE_ENV === 'production' ? ' error' : 'off',
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
     'comma-dangle': ['warn', 'always-multiline'],
   },
   plugins: [
