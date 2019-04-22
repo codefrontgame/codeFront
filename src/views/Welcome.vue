df609d08da6d30b71cd54968d784bbb100b0ea00
William Levén <gurgyar@gmail.com>
2019-04-22 20:34:44 +0200

    Remove video

--- src/views/Welcome.vue
+++ src/views/Welcome.vue
@@ -2,10 +2,10 @@
   <div class="welcome">
     <img src="assets/logo.png" class="logo">
     <h1>Välkommen till codeFront!</h1>
-    <video class="video center"  controls>
+    <!--<video class="video center"  controls>
       <source src="assets/video.mp4" type="video/mp4">
       Det går inte att visa videon i din webbläsare.
-    </video>
+    </video>-->
     <router-link to="Game" tag="button" class="button to-game">Till spelet!</router-link>
   </div>
 </template>
